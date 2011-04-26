086832c6de5c0a4e8647bdaa575067a680ad9526
Pablo Gil <pgilmon@gmail.com>
2011-04-26 08:18:41 +0200

    Added basic Android project structure

---
+++ src/android/pablogil/NoMolestar.java
@@ -0,0 +1,13 @@
+package android.pablogil;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NoMolestar extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
